e022ce2cdca9adbddbc0d9cbbb9452ba222e3c54
Sam McDavid <mcdavidsa@gmail.com>
2017-02-03 14:07:26 -0500

    API Don't need Live Reloader
    Why
    ---
    
    - Live reloading is for views, which we don't have or need.
    
    This change addresses the need by
    ---------------------------------
    
    - Yank out live reloading

--- apps/web_delivery/lib/web_delivery/endpoint.ex
+++ apps/web_delivery/lib/web_delivery/endpoint.ex
@@ -6,8 +6,6 @@ defmodule WebDelivery.Endpoint do
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
   end
 
