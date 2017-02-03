569cc0f42fba249a0aaaa2ad533bbee2a0a796a8
Sam McDavid <mcdavidsa@gmail.com>
2017-02-03 13:50:51 -0500

    Remove the Static Plugs
    Why
    ---
    
    - No need for static plugs anymore at all.
    
    This change addresses the need by
    ---------------------------------
    
    - Yank it out

--- apps/web_delivery/lib/web_delivery/endpoint.ex
+++ apps/web_delivery/lib/web_delivery/endpoint.ex
@@ -3,14 +3,6 @@ defmodule WebDelivery.Endpoint do
 
   socket "/socket", WebDelivery.UserSocket
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :web_delivery, gzip: false,
-    only: ~w(robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
